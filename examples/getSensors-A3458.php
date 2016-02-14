e7d3a22188cd6b0048ad17c3b6a4c5fa77398d9a
James Jarvis <jj@aprsworld.com>
2016-02-14 17:31:59 +0000

    Added log flag to pcwx encode

--- examples/getSensors-A3458.php
+++ examples/getSensors-A3458.php
@@ -28,36 +28,35 @@ $jd[$dname]['sensors']=array();
 $ws=pcwx_anemometer(0.765,0.350,$r[1]);
 $wg=pcwx_anemometer(0.765,0.350,$r[2]);
 $wa=pcwx_anemometer_average(0.765,0.350,$r[0],$r[43]/100.0);
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Wind Speed','m/s',$ws,'speed','m/s');
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Wind Gust','m/s',$wg,'speed','m/s');
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Wind Average','m/s',$wa,'speed','m/s');
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Wind Count','',$r[0]);
+$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Wind Speed','m/s',$ws,'speed','m/s',true);
+$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Wind Gust','m/s',$wg,'speed','m/s',true);
+$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Wind Average','m/s',$wa,'speed','m/s',true);
+$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Wind Count','',$r[0],'','',true);
 
 /* top of container / purple wires wind turbine */
 $rpm=pcwx_anemometer(12.0,0.0,$r[7]);
 $rpmGust=pcwx_anemometer(12.0,0.0,$r[8]);
 $rpmAverage=pcwx_anemometer_average(12.0,0.0,$r[6],$r[43]/100.0);
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Container Turbine Rotor Speed','RPM',$rpm);
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Container Turbine Rotor Gust','RPM',$rpmGust);
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Container Turbine Rotor Average','RPM',$rpmAverage);
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Container Turbine Rotor Count','',$r[6]);
+$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Container Turbine Rotor Speed','RPM',$rpm,'','',true);
+$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Container Turbine Rotor Gust','RPM',$rpmGust,'','',true);
+$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Container Turbine Rotor Average','RPM',$rpmAverage,'','',true);
+$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Container Turbine Rotor Count','',$r[6],'','',true);
 
 
 
 $jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Supply Voltage','volts',$r[19]*XR2G_VIN_V_PER_BIT);
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('12V Rectifier 1 Temperature','&deg;C',$r[27]*XRW2G_5VOLT_V_PER_BIT*100.0-273.15,'temperature','c');
+$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('12V Rectifier 1 Temperature','&deg;C',$r[27]*XRW2G_5VOLT_V_PER_BIT*100.0-273.15,'temperature','c',true);
 $jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Matrix Open Circuit Voltage','VDC',$r[31]*XRW2G_5VOLT_V_PER_BIT*20.0);
 
 /* meta data */
 $jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Modbus Host','','192.168.8.2:503');
 
 
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Sequence Number','',$r[42]);
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Ticks','seconds',$r[43]/100.0);
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Uptime','minutes',$r[44]);
+$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Sequence Number','',$r[42],'','',true);
+$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Ticks','seconds',$r[43]/100.0,'','',true);
+$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Uptime','minutes',$r[44],'','',true);
 
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Packet Date','UTC',date("Y-m-d H:i:s"));
-$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Linux Uptime','',exec("uptime"));
+$jd[$dname]['sensors'] += pcwx_encodeForBroadcast('Packet Date','UTC',date("Y-m-d H:i:s"),'','',true);
 
 /* send data to broadcast server(s) */
 print_r($jd);
--- lib/lib.pcwx.php
+++ lib/lib.pcwx.php
@@ -4,7 +4,7 @@ define('PCWX_VIN_V_PER_BIT',0.008544922);
 /* volts per bit for 0 to 5 volt input */
 define('PCWX_5VOLT_V_PER_BIT',0.001220703);
 
-function pcwx_encodeForBroadcast($title,$units,$val,$type='',$typeUnits='') {
+function pcwx_encodeForBroadcast($title,$units,$val,$type='',$typeUnits='',$log=false) {
 	$j=array();
 	$j[md5($title)]['title']=$title;
 	$j[md5($title)]['units']=$units;
@@ -13,6 +13,9 @@ function pcwx_encodeForBroadcast($title,$units,$val,$type='',$typeUnits='') {
 	if ( '' != $type && '' != $typeUnits ) {
 		$j[md5($title)]['type']=$type;
 		$j[md5($title)]['typeUnits']=$typeUnits;
+	}
+	if ( false != $log ) {
+		$j[md5($title)]['log']=true;
 	}
 
 	return $j;
